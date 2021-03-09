a62df5cbcdf761938ecae2e8ec2663f57673731c
Wasil D <sub_wasil@mail.ru>
2021-03-09 21:04:49 +0200

    add auth provider calling, .env config

--- apigateway.go
+++ apigateway.go
@@ -1,19 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func token(w http.ResponseWriter, r *http.Request) {
+
+	authProviderURL, exists := os.LookupEnv("AUTH_PROVIDER_URL")
+
+	if !exists {
+		log.Fatal("AUTH_PROVIDER_URL is not defined")
+	}
+
+	resp, err := http.Get(authProviderURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	fmt.Println("Response status:", resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(string(body))
+
+	if resp.StatusCode == 200 {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("token"))
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Wrong auth credentials"))
+	}
+}
+
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello World")
-    })
 
-    http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-        name := r.URL.Path[len("/greet/"):]
-        fmt.Fprintf(w, "Hello %s\n", name)
-    })
+	err := godotenv.Load()
 
-    http.ListenAndServe(":8888", nil)
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	http.HandleFunc("/token", token)
+
+	http.ListenAndServe(":8888", nil)
 }
